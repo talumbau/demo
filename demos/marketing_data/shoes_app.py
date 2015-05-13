9a928d67bc5ed857db49ccaae1000f186c63087a
talumbau <tj.alumbaugh@continuum.io>
2015-05-13 13:23:28 -0500

    shoes_app improvements
    custom scatter plot
    better tooltips

--- demos/marketing_data/shoes_app.py
+++ demos/marketing_data/shoes_app.py
@@ -5,6 +5,8 @@ See the README.md file in this directory for instructions on running.
 """
 
 import logging
+import random
+import bokeh
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -12,9 +14,11 @@ from os import listdir
 from os.path import dirname, join, splitext
 
 import numpy as np
+from numpy import pi
 import pandas as pd
+import shoes_func
 
-from bokeh.models import ColumnDataSource, Plot
+from bokeh.models import ColumnDataSource, Plot, HoverTool
 from bokeh.plotting import figure, curdoc
 from bokeh.charts import Bar, output_file, show, Histogram
 from bokeh.properties import String, Instance
@@ -22,8 +26,16 @@ from bokeh.server.app import bokeh_app
 from bokeh.server.utils.plugins import object_page
 from bokeh.models.widgets import HBox, VBox, VBoxForm, PreText, Select
 
+ranges = ["< $100", "< $200", "< $300", 
+          "< $400", "< $500", "< $600", "< $700", "< $800", "< $900", 
+          "< $1000", "< $1100", "< $1200", "< $1300", "< $1400", "< $1500", 
+          "< $1600", "< $1700", "< $1800", "< $1900", "< $2000", "< $2100", 
+          "< $2200", "< $2300", "< $2400", "< $2500", "< $2600", "< $2700", 
+          "< $2800", "< $2900", "< $3000", "< $3100", "< $3200", "< $3300", 
+          "< $3400", "< $3500", "< $3600", "< $3700", "< $3800", "< $3900", 
+          "< $4000", ">= $4000"]
+
 def get_shoe_data():
-    import shoes_func
     b2p = shoes_func.get_brands_to_prices()
     b2p_small = {}
     for count, k in zip(range(0,30), b2p):
@@ -34,14 +46,48 @@ b2p = get_shoe_data()
 #df = pd.DataFrame(b2p)
 brands = [k for k in zip(range(0,20), b2p.keys())]
 
-ranges = ["< $100", "< $200", "< $300", 
-          "< $400", "< $500", "< $600", "< $700", "< $800", "< $900", 
-          "< $1000", "< $1100", "< $1200", "< $1300", "< $1400", "< $1500", 
-          "< $1600", "< $1700", "< $1800", "< $1900", "< $2000", "< $2100", 
-          "< $2200", "< $2300", "< $2400", "< $2500", "< $2600", "< $2700", 
-          "< $2800", "< $2900", "< $3000", "< $3100", "< $3200", "< $3300", 
-          "< $3400", "< $3500", "< $3600", "< $3700", "< $3800", "< $3900", 
-          "< $4000", ">= $4000"]
+_shoedf = shoes_func.get_all_shoes()
+idx = (_shoedf['price'] + 100)//100
+idx = [int(x) + 1 for x in idx]
+counts = [0] * len(ranges)
+colors = bokeh.palettes.PRGn11
+xvals = []
+yvals = []
+xcat = []
+rightvals = []
+tops = []
+bottoms = []
+fills = []
+height = [0.5] * len(_shoedf['price'])
+width = [0.5] * len(_shoedf['price'])
+
+for i in _shoedf.index:
+    ans = _shoedf.ix[i]
+    idx = int(ans['price'] + 100)//100 + 1
+    print "idx is ", idx
+    print "price is ", ans['price']
+    #Categorical stuff
+    xcat.append(ranges[idx-2])
+    yvals.append(counts[idx] - 0.5*counts[idx])
+
+    #quad stuff
+    xvals.append(idx - 0.25)
+    rightvals.append(idx + 0.25)
+    bottoms.append(counts[idx]/2)
+    tops.append(counts[idx] + 0.5)
+    counts[idx] = counts[idx] + 1
+    fills.append(colors[(random.randint(0, 10000) % 11)])
+
+_shoedf['xvals'] = xvals
+_shoedf['xcat'] = xcat
+_shoedf['rightvals'] = rightvals
+_shoedf['bottoms'] = bottoms
+_shoedf['tops'] = tops
+_shoedf['fills'] = fills
+_shoedf['yvals'] = yvals
+_shoedf['height'] = height
+_shoedf['width'] = width
+
 
 def make_row(idxs, _len):
     x = np.zeros(_len)
@@ -62,6 +108,7 @@ class ShoeApp(VBox):
     hist_plot = Instance(Plot)
     # data source
     source = Instance(ColumnDataSource)
+    dfsource = Instance(ColumnDataSource)
 
     # input
     selectr = Instance(Select)
@@ -87,9 +134,11 @@ class ShoeApp(VBox):
         obj.bottomrow = HBox()
         obj.totalbox = VBox()
 
+        obj.make_source()
         # outputs
         obj.make_inputs()
-        obj.make_plots()
+        #obj.make_plots()
+        obj.make_better_plots()
 
         # layout
         obj.set_children()
@@ -105,6 +154,7 @@ class ShoeApp(VBox):
 
     def make_source(self):
         self.source = ColumnDataSource(data=self.df)
+        self.dfsource = ColumnDataSource(data=self.shoedf)
 
     def make_inputs(self):
 
@@ -123,6 +173,51 @@ class ShoeApp(VBox):
                   legend="top_right")
 
         self.plot = bar
+        self.make_hist_plot()
+
+    def make_better_plots(self):
+        
+        """sdf = self.shoedf
+        idx = (sdf['price'] + 100)//100
+        idx = [int(x) + 1 for x in idx]
+        counts = [0] * len(ranges)
+        colors = ["blue", "orange", "green", "yellow", "purple"]
+        xvals = []
+        rightvals = []
+        bottoms = []
+        fills = []
+        height = [1] * len(sdf['price'])
+
+        for i in sdf.index:
+            ans = sdf.ix[i]
+            idx = int(ans['price'] + 100)//100 + 1
+            print "idx is ", idx
+            print "price is ", ans['price']
+            xvals.append(idx - 0.25)
+            rightvals.append(idx + 0.25)
+            bottoms.append(counts[idx])
+            counts[idx] = counts[idx] + 1
+            fills.append(colors[idx % 5])"""
+
+        TOOLS = "box_select,lasso_select"
+        tooltips = "<span class='tooltip-text'>Name: @name</span>\n<br>"
+        tooltips += "<span class='tooltip-text'>Brand: @brand</span>\n<br>"
+        tooltips += "<span class='tooltip-text'>Price: @price</span>\n<br>"
+        #hover = HoverTool(tooltips="@num_reviews")
+        #hover = HoverTool(tooltips="@names")
+        hover = HoverTool(tooltips=tooltips)
+
+        #p = figure(tools=TOOLS, width=1100, height=700, x_range=x_rr, y_range=y_rr, title="Price Distribution")
+        #p = figure(tools=TOOLS, width=1100, height=700, title="Price Distribution")
+        #p = figure(tools=TOOLS, width=1100, height=700, x_range=ranges, title="Price Distribution", angle=pi/4)
+        p = figure(tools=TOOLS, width=1100, height=700, x_range=ranges, title="Price Distribution")
+        p.xaxis.major_label_orientation = pi/4
+
+        #p.quad(left='xvals', right='rightvals', top='tops', bottom='bottoms', color='fills', source=self.dfsource)
+        p.rect(x='xcat', y='yvals', width='width', height='height', color='fills', source=self.dfsource)
+        p.add_tools(hover)
+
+        self.plot = p
         self.make_hist_plot()
 
     def make_hist_plot(self):
@@ -144,7 +239,7 @@ class ShoeApp(VBox):
         )"""
 
         
-        if self.selectr.value is None or self.selectr.value == 'Choose A State':
+        if self.selectr.value is None or self.selectr.value == 'Choose A Brand':
             p = figure(
                 title="Brand histogram",
                 plot_width=800, plot_height=400,
@@ -172,7 +267,7 @@ class ShoeApp(VBox):
     def input_change(self, obj, attrname, old, new):
 
         self.make_source()
-        self.make_plots()
+        self.make_better_plots()
         self.set_children()
         curdoc().add(self)
 
@@ -191,6 +286,10 @@ class ShoeApp(VBox):
     @property
     def df(self):
         return get_shoe_data()
+
+    @property
+    def shoedf(self):
+        return _shoedf
 
 
 # The following code adds a "/bokeh/shoes/" url to the bokeh-server. This URL
--- demos/marketing_data/shoes_func.py
+++ demos/marketing_data/shoes_func.py
@@ -4,6 +4,7 @@ import collections
 from bokeh.sampledata.iris import flowers
 from bokeh.plotting import *
 import itertools
+import pandas as pd
 
 all_brands = set()
 all_mns = set()
@@ -18,56 +19,99 @@ lines1 = [json.loads(line) for line in open('/Users/talumbau/Downloads/116_1.txt
 lines2 = [json.loads(line) for line in open('/Users/talumbau/Downloads/116_2.txt')]
 all_ps = []
 
+def extract_full_price(r):
+    if 'price' in r:
+        p = r['price']
+    else:
+        p = r['salePrice']
+
+    return extract_price(p)
+
+
+def extract_price(p):
+    p = p.strip()
+    p = re.sub('[,&?#]', '', p)
+    if p.startswith("USD"):
+        p = p[3:]
+    p = p.strip()
+    if p.startswith("GBP"):
+        p = p[3:]
+    if p.startswith("AUD"):
+        p = p[3:]
+    p = p.strip()
+    p = p.strip()
+    if not p:
+        return 0.0
+    try:
+        return float(p)
+    except ValueError:
+        print "this is no good: ", p
+        idx = p.find('.')
+        try:
+            return float(p[:idx+3])
+        except ValueError:
+            print "this is also no good: ", p
+            if ';' in p:
+                p = p[p.find(';')+1:]
+            return float(p[:idx+3])
+
+def collect(lines):
+    global num_mans
+    global num_mannums
+    global num_prices
+    global num_brands
+    for x in lines:
+        if 'manufacturer' in x:
+            num_mans += 1
+        if 'manufacturerNumber' in x:
+            num_mannums += 1
+            all_mns.add(x['manufacturerNumber'])
+        if 'prices' in x:
+            num_prices += 1
+        if 'brand' in x:
+            num_brands += 1
+            all_brands.add(x['brand'].lower())
+        if 'prices' in x and 'brand' in x:
+            goodones.append(x)
+
+
+def price_is_good(r):
+
+    if 'price' in r['prices'][0] or 'salePrice' in r['prices'][0]:
+        return True
+    else:
+        return False
+
+def get_all_shoes():
+    thelines = (json.loads(line) for line in open('/Users/talumbau/Downloads/116_1.txt'))
+    #lines2 = [json.loads(line) for line in open('/Users/talumbau/Downloads/116_2.txt')]
+    return _get_all_shoes(thelines)
+
+
+def _get_all_shoes(alllines):
+    name = []
+    price = []
+    brand = []
+
+    fields = ["name", "prices", "brand"]
+    for x in alllines:
+        checks = [f in x for f in fields]
+        has_all = all(checks)
+        if has_all and price_is_good(x):
+            theprice = extract_full_price(x['prices'][0])
+            thebrand = x['brand'].lower()
+            name.append(x['name'])
+            price.append(theprice)
+            brand.append(thebrand)
+
+    df = pd.DataFrame({'name':name, 'price':price, 'brand':brand})
+    return df.loc[:300, :]
+
+
 def get_brands_to_prices():
 
     global has_prices
     brands_to_prices = collections.defaultdict(list)
-
-    def extract_price(p):
-        p = p.strip()
-        p = re.sub('[,&?#]', '', p)
-        if p.startswith("USD"):
-            p = p[3:]
-        p = p.strip()
-        if p.startswith("GBP"):
-            p = p[3:]
-        if p.startswith("AUD"):
-            p = p[3:]
-        p = p.strip()
-        p = p.strip()
-        if not p:
-            return 0.0
-        try:
-            return float(p)
-        except ValueError:
-            print "this is no good: ", p
-            idx = p.find('.')
-            try:
-                return float(p[:idx+3])
-            except ValueError:
-                print "this is also no good: ", p
-                if ';' in p:
-                    p = p[p.find(';')+1:]
-                return float(p[:idx+3])
-
-    def collect(lines):
-        global num_mans
-        global num_mannums
-        global num_prices
-        global num_brands
-        for x in lines:
-            if 'manufacturer' in x:
-                num_mans += 1
-            if 'manufacturerNumber' in x:
-                num_mannums += 1
-                all_mns.add(x['manufacturerNumber'])
-            if 'prices' in x:
-                num_prices += 1
-            if 'brand' in x:
-                num_brands += 1
-                all_brands.add(x['brand'].lower())
-            if 'prices' in x and 'brand' in x:
-                goodones.append(x)
 
     collect(lines1)
     collect(lines2)
